5613589e5523ce3bfc6cc602f2731b5b570a0f0a
Yuriy Chornopyskyi <yurii.chornopyskyi@intellias.com>
2022-07-05 11:39:06 +0300

    update app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <div>
+    <h1>VRS Logs</h1>
+    <p v-if="!logs">Loading...</p>
+    <pre v-else>{{ logs }}</pre>
+  </div>
 </template>
 
 <script>
@@ -7,8 +11,21 @@ export default {
   name: "App",
   data() {
     return {
-      value: "World"
+      value: "World",
+      message: "",
+      todoId: 1,
+      logs: null
     };
+  },
+  methods: {
+    async getAllLogs() {
+      this.logs = null
+      const res = await fetch(`https://localhost:7108/api/vrslog`)
+      this.logs = await res.json()
+    }
+  },
+  mounted() {
+    this.getAllLogs()
   }
 };
 </script>
